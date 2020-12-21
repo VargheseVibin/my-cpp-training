e3319c08eab466532a2da06c631c2305eafbfc35
Vibin Varghese <vibinvvarghese@gmail.com>
2020-12-21 21:23:35 +0530

    added Function sample

---
+++ fn_area.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std ;
+
+const double  PI {3.14159};
+
+double calcAreaCircle (double radius) {
+    double area {PI*radius*radius};
+    return area ;
+}
+
+double calcVolumeCylinder(double radius, double height) {
+    double volume {calcAreaCircle(radius)*height};
+    return volume;
+}
+
+void areaCircle() {
+    double radius {};
+    cout << "Enter the radius of the circle in (cm):" ;
+    cin >> radius ;
+
+    cout << "The Area of the circle is :" << calcAreaCircle(radius) << " sq. cms" << endl ;
+}
+
+
+void volumeCylinder() {
+    double radius{};
+    double height{};
+    cout << "Enter the radius of the cylinder in (cm):" ;
+    cin >> radius ;
+    cout << "Enter the radius of the height in (cm):" ;
+    cin >> height ;
+    cout << "The volume of the cylinder is:" << calcVolumeCylinder(radius,height) << " cu.cms" << endl ; 
+}
+
+
+int main () {
+
+    areaCircle();
+    volumeCylinder();
+
+    return 0 ;
+
+}
