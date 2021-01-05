409b292c84434a6e5377501ce370e2b3896e9452
Vibin Varghese <vibinvvarghese@gmail.com>
2021-01-05 14:25:35 +0530

    updates

---
+++ cstyle_str.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std ;
+
+int main () {
+    char *firstName {"Vibin"};
+    
+    cout << "firstName:" << firstName << endl ; 
+    cout << "*firstName:" << *firstName << endl ;
+    cout << "*(firstName+1)" << *(firstName+1) << endl ; 
+    cout << "*(firstName+2)" << *(firstName+2) << endl ; 
+    cout << 
+
+}
